01e40e2c07b5b7f332765af4f5b5bc32187721f6
Vitalii Samchynskyi <samuelvit88@gmail.com>
2025-12-07 17:04:38 +0200

    Add project files.

---
+++ AbstractClassesAndInterfaces/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace AbstractClassesAndInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 10, 20, 30, 40 };
+            MyArray arr = new MyArray(data);
+            arr.Show();
+            arr.Show("This is my array");
+        }
+    }
+
+      
+
+}
