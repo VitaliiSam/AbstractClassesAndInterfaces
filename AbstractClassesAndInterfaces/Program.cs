dc273d1a79d5072d816bed3fdcef7c9c92515a0a
Vitalii Samchynskyi <samuelvit88@gmail.com>
2025-12-07 18:02:22 +0200

    Added second interface IMath

--- AbstractClassesAndInterfaces/Program.cs
+++ AbstractClassesAndInterfaces/Program.cs
@@ -9,8 +9,16 @@ namespace AbstractClassesAndInterfaces
         {
             int[] data = { 10, 20, 30, 40 };
             MyArray arr = new MyArray(data);
+            //Output
             arr.Show();
             arr.Show("This is my array");
+            //Math: Max, Min, Avg, Search
+            Console.WriteLine("Math interface");
+            Console.WriteLine(arr.Max());
+            Console.WriteLine(arr.Min());
+            Console.WriteLine(arr.Avg());
+            Console.WriteLine("Search value: 30 - " + arr.Search(30));
+            Console.WriteLine("Search value: 90 - " + arr.Search(90));
         }
     }
 
---
+++ ClassLibrary1/Tasks.cs
@@ -0,0 +1,87 @@
+﻿using System.Globalization;
+
+namespace ClassLibrary1
+{
+    //IOutuput interface
+    public interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+    //IMath interface
+    public interface IMath
+    {
+        int Max();
+        int Min();
+        float Avg();
+        bool Search(int valueToSearch);
+    }
+    public class MyArray : IOutput, IMath
+    {
+        private int[] numbers;
+        public MyArray (int[] numbers)
+        {
+            this.numbers = numbers;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Elements of arrays");
+            foreach (int i in numbers)
+            {  
+                Console.WriteLine(i);
+            }
+
+        }
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+            Show();
+        }
+        //IMath
+        public int Max()
+        {
+            int max = numbers[0];
+            foreach (int n in numbers)
+            {
+                if(n > max)
+                {
+                    max = n;
+                }    
+            }
+            return max;
+        }
+        public int Min()
+        {
+            int min = numbers[0];
+            foreach (int n in numbers)
+            {
+                if (n < min)
+                {
+                    min = n;
+                }
+            }
+            return min;
+        }
+        public float Avg()
+        {
+            int sum = numbers[0];
+            foreach(int n in numbers)
+            { 
+                sum += n; 
+            }
+            return (float)sum / numbers.Length;
+
+        }
+        public bool Search(int valueToSearch)
+        {
+            foreach (int n in numbers)
+            {
+                if(n == valueToSearch)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }        
+}
